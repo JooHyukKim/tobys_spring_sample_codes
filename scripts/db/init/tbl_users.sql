42aa46a9ae3fb21d9e53896af4a405230bfd941a
김주혁 <beanskobe@gmail.com>
2021-10-11 14:32:53 +0900

    FEAT : MySQL docker container compose파일 셍성 및 초기 테이블 생성 스크립트 적용

---
+++ scripts/db/init/tbl_users.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE mydb;
+
+CREATE TABLE mydb.users(
+                          id VARCHAR(100) PRIMARY KEY,
+                          name VARCHAR (100),
+                          password VARCHAR (100)
+);
