2faa643fcfcc9dee6fe373f7c50f600285d03333
김주혁 <beanskobe@gmail.com>
2021-10-14 20:10:01 +0900

    FEAT: 어노테이션 기반 트랜잭션 적용

--- src/main/java/user/springbook/service/UserService.java
+++ src/main/java/user/springbook/service/UserService.java
@@ -1,20 +1,26 @@
 package user.springbook.service;
 
+import org.springframework.transaction.annotation.Transactional;
 import user.springbook.domain.User;
 
 import java.sql.SQLException;
 import java.util.List;
 
+@Transactional
 public interface UserService {
     void add(User user);
-
-    User get(String id);
-
-    List<User> getAll();
 
     void deleteAll();
 
     void update(User user);
 
     void upgradeLevels() throws SQLException;
+
+    @Transactional(readOnly = true)
+    User get(String id);
+
+
+    @Transactional(readOnly = true)
+    List<User> getAll();
+
 }
